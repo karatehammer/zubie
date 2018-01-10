2bdd717786ff593eb4254f835e61734f55d7fca1
Nick Hammer <nhammer@gmail.com>
2018-01-10 13:48:07 -0600

    played around with reducing the gebconfig file quite a bit and just general getting the browser to launch and navigate properly

--- src/test/groovy/com.example/specs/ZubieHomePageSpec.groovy
+++ src/test/groovy/com.example/specs/ZubieHomePageSpec.groovy
@@ -1,40 +1,35 @@
 package com.example.specs
 
-import com.example.pages.ZubieHomePage
+import com.example.pages.GoogleHomePage
 import geb.spock.GebReportingSpec
 import org.junit.Test
 
 
+
 class ZubieHomePageSpec extends GebReportingSpec {
-
-
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
-        return super.clone()
-    }
 
     @Test
     def "Navigate to the Zubie Page"() {
 
         when:
-        to ZubieHomePage
+        to GoogleHomePage
 
         then:
-        assert ZubieHomePage.url.contains("zubie")
+        to GoogleHomePage
+
     }
 
-
-    @Test
-    def "Navigate to login"() {
-
-        when:
-        ZubieHomePage zubieHomePage = to ZubieHomePage
-
-        then:
-        zubieHomePage.loginLink.click()
-
-        and: "Validate the the login page has loaded"
-        browser.currentUrl.contains("my.zubie.co")
-    }
+//    @Test
+//    def "Navigate to login"() {
+//
+//        when:
+//        ZubieHomePage zubieHomePage = to ZubieHomePage
+//
+//        then:
+//        zubieHomePage.loginLink.click()
+//
+//        and: "Validate the the login page has loaded"
+//        browser.currentUrl.contains("my.zubie.co")
+//    }
 
 }
--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -4,51 +4,18 @@
 	See: http://www.gebish.org/manual/current/#configuration
 */
 
-
-import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
-import org.openqa.selenium.chrome.ChromeOptions
-import org.openqa.selenium.firefox.FirefoxDriver
+
 
 waiting {
 	timeout = 2
 }
 reportsDir = new File("build/reports")
 
-environments {
+baseUrl = "http://www.google.com"
 
-	// run via “./gradlew chromeTest”
-	// See: http://code.google.com/p/selenium/wiki/ChromeDriver
-	chrome {
-		def chromeDriver = new File('src/test/resources/chromedriver.exe')
-        ChromeOptions options = new ChromeOptions()
-        options.addArguments("start-maximized")
-		System.setProperty('webdriver.chrome.driver', chromeDriver.absolutePath)
-		def driverInstance = { new ChromeDriver(options) }
-        driverInstance.executeScript("")
-        WebDriver.resiz
-	}
-
-	// run via “./gradlew chromeHeadlessTest”
-	// See: http://code.google.com/p/selenium/wiki/ChromeDriver
-	chromeHeadless {
-		driver = {
-			ChromeOptions o = new ChromeOptions()
-			o.addArguments('headless')
-			new ChromeDriver(o)
-		}
-	}
-
-	// run via “./gradlew firefoxTest”
-	// See: http://code.google.com/p/selenium/wiki/FirefoxDriver
-	firefox {
-		atCheckWaiting = 1
-
-		driver = { new FirefoxDriver() }
-	}
-
+driver = {
+	def chromeDriverPlace = new File('src/test/resources/chromedriver.exe')
+	System.setProperty('webdriver.chrome.driver', chromeDriverPlace.absolutePath)
+    new ChromeDriver()
 }
-
-// To run the tests with all browsers just run “./gradlew test”
-//
-//baseUrl = "http://gebish.org"
